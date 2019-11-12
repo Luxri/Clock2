2120ecc5d3eae1bb043ef335a33aa743fd026cd6
Luxri <lucas.nasstrom@elev.ga.ntig.se>
2019-11-12 10:46:21 +0100

    Initial commit

---
+++ src/Clock.java
@@ -0,0 +1,36 @@
+public class Clock{
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private String displayString;
+
+    public Clock () {
+        this.hours = new NumberDisplay(24);
+        this.minutes = new NumberDisplay(60);
+        updateDisplay();
+    }
+
+    public Clock(int hours, int minutes) {
+        this();
+        this.hours.setValue(0);
+        this.minutes.setValue(0);
+    }
+    public void timeTick () {
+        minutes.increment();
+    }
+
+    public void setTime(int hours, int minutes) {
+        this.hours.setValue(hours);
+        this.minutes.setValue(minutes);
+    }
+    public String getTime() {
+        return displayString;
+    }
+    private void updateDisplay() {
+        displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
+    }
+
+    public static void main(String[] args) {
+        Clock c1 = new Clock();
+        System.out.println(c1.getTime());
+    }
+}
