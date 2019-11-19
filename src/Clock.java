749c1da21c784d28b99eb18be0624df951ad1f0c
Luxri <lucas.nasstrom@elev.ga.ntig.se>
2019-11-19 10:17:18 +0100

    javadoc

--- src/Clock.java
+++ src/Clock.java
@@ -3,11 +3,21 @@ public class Clock{
     private NumberDisplay minutes;
     private String displayString;
 
+
+    /**
+     * Konstruktor
+     */
     public Clock () {
         this.hours = new NumberDisplay(24);
         this.minutes = new NumberDisplay(60);
         updateDisplay();
     }
+
+    /**
+     * Parameter för hours/minutes, sätts till 00:00
+     * @param hours
+     * @param minutes
+     */
 
     public Clock(int hours, int minutes) {
         this();
@@ -18,13 +28,28 @@ public class Clock{
         minutes.increment();
     }
 
+    /**
+     * Setter för tiden
+     * @param hours
+     * @param minutes
+     */
+
     public void setTime(int hours, int minutes) {
         this.hours.setValue(hours);
         this.minutes.setValue(minutes);
     }
+
+    /**
+     * Getter för displayString
+     * @return
+     */
     public String getTime() {
         return displayString;
     }
+
+    /**
+     * Displaystringen och uppdatering av display
+     */
     private void updateDisplay() {
         displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
     }
--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -1,15 +1,29 @@
+
 public class NumberDisplay {
     protected int limit;
     protected int value;
 
-
+    /**
+     * Limit
+     * @param maxLimit
+     */
     public NumberDisplay(int maxLimit) {
         value = 0;
         limit = maxLimit;
     }
+
+    /**
+     * getter för value
+     * @return
+     */
     public int getValue() {
         return value;
     }
+
+    /**
+     * Failsafe för klockan så inget olgiltigt värde kommer in
+     * @param newValue
+     */
     public void setValue(int newValue) {
         if (newValue < 0 && newValue > limit) {
 
@@ -17,6 +31,11 @@ public class NumberDisplay {
             this.value = newValue;
         }
     }
+
+    /**
+     * Visar displayen
+     * @return
+     */
     public String getDisplayValue() {
         String display = Integer.toString(value);
         if (value < 10) {
@@ -24,6 +43,10 @@ public class NumberDisplay {
         }
         return display;
     }
+
+    /**
+     * Adderar minuter
+     */
     public void increment() {
         value++;
         if (value == limit) {
